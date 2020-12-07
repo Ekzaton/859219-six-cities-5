903679ceaed7a1f5e8a3dcb79a440a00410e0bac
Ekzaton <ekzaton@gmail.com>
2020-12-07 13:55:10 +0500

    Created 'offers-item.tsx' component

---
+++ src/components/offers-item/offers-item.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+
+import {offerType} from "../../types";
+import {getRatingStars} from "../../utils";
+
+interface Props {
+  offer: offerType;
+}
+
+const OffersItem: React.FunctionComponent<Props> = (props: Props) => {
+  const {offer} = props;
+  const favoriteClass = offer.isFavorite && `place-card__bookmark-button--active`;
+
+  return (
+    <article className="cities__place-card place-card">
+      {offer.isPremium &&
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      }
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={offer.previewImage} width="260" height="200" alt="Place image"/>
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{offer.price}</b>
+            <span className="place-card__price-text">&nbsp;&#47;&nbsp;night</span>
+          </div>
+          <button className={`place-card__bookmark-button ${favoriteClass} button`} type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={getRatingStars(offer.rating)}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{offer.title}</a>
+        </h2>
+        <p className="place-card__type">{offer.type}</p>
+      </div>
+    </article>
+  );
+};
+
+export default OffersItem;
