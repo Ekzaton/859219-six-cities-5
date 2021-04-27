063286ae1d51fe4c7c43a96e0bd85302b85583ff
Ekzaton <ekzaton@gmail.com>
2021-04-27 23:16:40 +0500

    Updated 'index.tsx' component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import offers from "./mocks/offers";
-import reviews from "./mocks/reviews";
+import {offers} from "./mocks/offers";
+import {reviews} from "./mocks/reviews";
 
 import App from "./components/app/app";
 
