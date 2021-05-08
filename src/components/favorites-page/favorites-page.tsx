accb16d5aa9bfc32831ef6674a80d012fe56e9ff
Ekzaton <ekzaton@gmail.com>
2021-05-08 16:58:29 +0500

    Updated components

--- src/components/favorites-page/favorites-page.tsx
+++ src/components/favorites-page/favorites-page.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import {offerType} from "../../types";
 import {getRatingStars} from "../../utils";
 
+import PageHeader from "../page-header/page-header";
+
 type Props = {
   offers: offerType[];
 }
@@ -12,28 +14,7 @@ const FavoritesPage: React.FunctionComponent<Props> = (props: Props) => {
 
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <PageHeader/>
 
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
