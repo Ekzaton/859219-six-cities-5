0790a14a2b86d17cf450090321abdd991811f0cb
Ekzaton <ekzaton@gmail.com>
2021-05-30 11:57:19 +0500

    Updated 'app.tsx' component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -6,6 +6,7 @@ import {Offer, Review} from "../types";
 import FavoritesPage from "../favorites-page/favorites-page";
 import MainPage from "../main-page/main-page";
 import OfferPage from "../offer-page/offer-page";
+import PrivateRoute from "../private-route/private-route";
 import SignInPage from "../sign-in-page/sign-in-page";
 
 type Props = {
@@ -19,17 +20,9 @@ const App: React.FunctionComponent<Props> = (props: Props) => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={`/`}>
-          <MainPage/>
-        </Route>
-        <Route exact path={`/login`}>
-          <SignInPage/>
-        </Route>
-        <Route exact path={`/favorites`}>
-          <FavoritesPage
-            offers={offers}
-          />
-        </Route>
+        <Route exact path={`/`} component={MainPage}/>
+        <Route exact path={`/login`} component={SignInPage}/>
+        <PrivateRoute exact path={`/favorites`} render={() => <FavoritesPage/>}/>
         <Route exact path={`/offer/:id`}>
           <OfferPage
             offers={offers}
