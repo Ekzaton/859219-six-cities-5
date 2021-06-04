740cb49edda6b39ad71488699891c2dd052a4cd3
Ekzaton <ekzaton@gmail.com>
2021-06-04 23:08:31 +0500

    Connected history

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {Router, Switch, Route} from "react-router-dom";
+
+import history from "../../history/index";
 
 import FavoritesPage from "../favorites-page/favorites-page";
 import MainPage from "../main-page/main-page";
@@ -9,14 +11,14 @@ import SignInPage from "../sign-in-page/sign-in-page";
 
 const App: React.FunctionComponent = () => {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <Switch>
         <Route exact path={`/`} component={MainPage}/>
         <Route exact path={`/login`} component={SignInPage}/>
         <PrivateRoute exact path={`/favorites`} render={() => <FavoritesPage/>}/>
         <Route exact path={`/offer/:id`} component={OfferPage}/>
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 };
 
