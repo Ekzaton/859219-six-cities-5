12b866eae265f10339c8b4e4c10766501c5195e3
Ekzaton <ekzaton@gmail.com>
2021-06-14 11:20:26 +0500

    Updated api

--- src/services/const.ts
+++ src/services/const.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = `https://5.react.pages.academy/six-cities`;
+export const BASE_URL = `https://6.react.pages.academy/six-cities`;
 export const REQUEST_TIMEOUT = 5000;
 
 export enum HTTPCode {
