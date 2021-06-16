1d768877d5197324af20096fbc3cb404368d9629
Ekzaton <ekzaton@gmail.com>
2021-06-16 16:05:53 +0500

    Fix

--- src/utils.ts
+++ src/utils.ts
@@ -16,18 +16,18 @@ export const getSortedOffers = (currentSorting: SortingType, offers: Offer[]): O
     case SortingType.POPULAR:
       return [...offers];
     case SortingType.PRICE_HIGH_TO_LOW:
-      return [...offers].sort((offer1, offer2) => offer2.price - offer1.price);
+      return [...offers.sort((offer1, offer2) => offer2.price - offer1.price)];
     case SortingType.PRICE_LOW_TO_HIGH:
-      return [...offers].sort((offer1, offer2) => offer1.price - offer2.price);
+      return [...offers.sort((offer1, offer2) => offer1.price - offer2.price)];
     case SortingType.TOP_RATED_FIRST:
-      return [...offers].sort((offer1, offer2) => offer2.rating - offer1.rating);
+      return [...offers.sort((offer1, offer2) => offer2.rating - offer1.rating)];
     default:
       return offers;
   }
 };
 
 export const getSortedReviews = (reviews: Review[]): Review[] =>
-  [...reviews].sort((review1, review2) => new Date(review2.date).getTime() - new Date(review1.date).getTime());
+  [...reviews.sort((review1, review2) => new Date(review2.date).getTime() - new Date(review1.date).getTime())];
 
 export const updateFavoriteOffers = (offers: Offer[], updatedOffer: Offer): Offer[] => {
   const index = offers.findIndex((offer) => offer.id === updatedOffer.id);
