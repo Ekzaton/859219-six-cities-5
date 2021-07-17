5717bec5b1a4454c5813da51504e9ab77d8245d9
Ekzaton <ekzaton@gmail.com>
2021-07-17 14:34:16 +0500

    Fix

--- src/components/error-page/error-page.tsx
+++ src/components/error-page/error-page.tsx
@@ -18,7 +18,12 @@ const ErrorPage = (props: ErrorPageProps): JSX.Element => {
         style={{marginTop: `35vh`}}
       >
         {loadingError
-          ? <>{loadingError.data.error ? loadingError.data.error : `Sorry cant find that!`}</>
+          ? <>
+            {loadingError.data.error
+              ? loadingError.data.error
+              : `Sorry cant find that!`
+            }
+          </>
           : `Sorry cant find that!`
         }
       </h1>
